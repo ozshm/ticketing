a2dcb56401c6994f22d00938667180bde7085e2c
Oz Shmuel <oz.shmuel@ibm.com>
2022-10-23 16:56:58 +0300

    section 20 complete

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,24 +1,26 @@
-import { Listener, OrderCreatedEvent, Subjects } from "@osticketing/common";
-import { Message } from "node-nats-streaming";
-import { queueGroupName } from "./queue-group-name";
-import { Ticket } from "../../models/ticket";
-import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@osticketing/common';
+import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  async onMessage (data: OrderCreatedEvent['data'], msg: Message) {
-    // find the ticket the order is reserving
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving
     const ticket = await Ticket.findById(data.ticket.id);
-    // if no ticket, throw an error
+
+    // If no ticket, throw error
     if (!ticket) {
       throw new Error('Ticket not found');
     }
 
-    // mark the ticket as being reserved by setting its ovrderId property
+    // Mark the ticket as being reserved by setting its orderId property
     ticket.set({ orderId: data.id });
 
-    // save the ticket
+    // Save the ticket
     await ticket.save();
     await new TicketUpdatedPublisher(this.client).publish({
       id: ticket.id,
@@ -26,8 +28,8 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
       title: ticket.title,
       userId: ticket.userId,
       orderId: ticket.orderId,
-      version: ticket.version
-    })
+      version: ticket.version,
+    });
 
     // ack the message
     msg.ack();
